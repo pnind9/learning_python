3d483f745f200e4d858a66873c619827b554011f
ixisnine_garfield <ixisnine@gmail.com>
2016-02-18 08:45:50 -0600

    new lesson work from PC

---
+++ Second_half/ex34.py
@@ -0,0 +1,6 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print "The animal at 1 is 'Python': %s" % animals[1]
+print "The third animal is 'Peacock': %s" % animals[2]
+print "The first animal is 'bear': %s" % animals[0]
+print "The animal at 3 is 'kangaroo': %s" % animals[3]
